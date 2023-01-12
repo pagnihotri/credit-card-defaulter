9010a06a2918a83cdacd3ce1ca05933d0f681ddb
pagnihotri <prash.agnihotri@gmail.com>
2023-01-12 14:47:07 +0530

    first version of code

---
+++ data_dump.py
@@ -0,0 +1,24 @@
+import pymongo
+import pandas as pd
+import json
+
+# Provide the mongodb localhost url to connect python to mongodb.
+#client = pymongo.MongoClient("mongodb+srv://pagnihotri:root@cluster0.asd2aoy.mongodb.net/?retryWrites=true&w=majority")
+#DATAFILE_PATH="https://raw.githubusercontent.com/avnyadav/sensor-fault-detection/main/aps_failure_training_set1.csv"
+#DATABASE_NAME='aps'
+#COLLECTION_NAME='sensor'
+
+#DATA_FILE_PATH="/config/workspace/aps_failure_training_set1.csv"
+client = pymongo.MongoClient("mongodb://localhost:27017/neurolabDB")
+DATA_FILE_PATH='/config/workspace/UCI_Credit_Card.csv'
+DATABASE_NAME="creditcard"
+COLLECTION_NAME="defaulter"
+
+if __name__=="__main__":
+    df=pd.read_csv(DATA_FILE_PATH)
+    print(f'The number of wors and columns {df.shape}')
+    df.reset_index(drop=True,inplace=True)
+    df.drop(columns=['ID'],inplace=True)
+    json_record=list(json.loads(df.T.to_json()).values())
+    print(json_record[0])
+    client[DATABASE_NAME][COLLECTION_NAME].insert_many(json_record)
