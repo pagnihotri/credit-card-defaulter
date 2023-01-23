d882d59b1c8004af46733ef025c00c7f2605b438
pagnihotri <prash.agnihotri@gmail.com>
2023-01-23 13:24:30 +0530

    updated batch prediction and main file

--- main.py
+++ main.py
@@ -8,7 +8,7 @@ from defaulter.pipeline.training_pipeline import start_training_pipeline
 if __name__=="__main__":
      try:
           start_training_pipeline()
-          #output_file = start_batch_prediction(input_file_path=file_path)
-          #print(output_file)
+          output_file = start_batch_prediction(input_file_path=file_path)
+          print(output_file)
      except Exception as e:
           print(e)
