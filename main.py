1f53baf071d91bda765be6c63d583010408dacc3
pagnihotri <prash.agnihotri@gmail.com>
2023-01-23 14:43:54 +0530

    updated

--- main.py
+++ main.py
@@ -1,9 +1,9 @@
 from defaulter.pipeline.training_pipeline import start_training_pipeline
-#from defaulter.pipeline.batch_prediction import start_batch_prediction
+from defaulter.pipeline.batch_prediction import start_batch_prediction
 
 
 
-#file_path="/config/workspace/UCI_Credit_Card.csv"
+file_path="/config/workspace/UCI_Credit_Card.csv"
 #print(__name__)
 if __name__=="__main__":
      try:
